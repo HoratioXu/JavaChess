a6683cae5b28f29a15f12c04f9bb2576400ae0ed
horatio xu <a2589654@gmail.com>
2019-10-24 01:22:07 -0700

    Tile gui and Piece icon

--- src/com/chess/gui/Table.java
+++ src/com/chess/gui/Table.java
@@ -1,25 +1,47 @@
 package com.chess.gui;
 
+import com.chess.engine.board.Board;
+import com.chess.engine.board.BoardUtils;
+
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Table {
     private final JFrame gameFrame;
-    private static Dimension OUTER_FRAME_DIMENSION = new Dimension(600,600);
+    private final BoardPanel boardPanel;
+    private final Board chessBoard;
+
+    private final static Dimension OUTER_FRAME_DIMENSION = new Dimension(600,600);
+    private final static Dimension BOARD_PANEL_DIMENSION = new Dimension(400,350);
+    private final static Dimension TILE_PANEL_DIMENSION = new Dimension(10,10);
+    private static String defaultPieceIconPath = "art/pieces/";
+    private final Color lightTileColor = Color.decode("#E4DDD1");
+    private final Color darkTileColor = Color.decode("#8E6D5D");
 
     public Table() {
         this.gameFrame = new JFrame("JavaChess");
-        final JMenuBar tableMenuBar = new JMenuBar();
-        populateMenuBar(tableMenuBar);
+        this.gameFrame.setLayout(new BorderLayout());
+        final JMenuBar tableMenuBar = createTableMenuBar();
         this.gameFrame.setJMenuBar(tableMenuBar);
         this.gameFrame.setSize(OUTER_FRAME_DIMENSION);
+        this.chessBoard = Board.createStandardBoard();
+        this.boardPanel = new BoardPanel();
+        this.gameFrame.add(this.boardPanel, BorderLayout.CENTER);
         this.gameFrame.setVisible(true);
     }
 
-    private void populateMenuBar(final JMenuBar tableMenuBar) {
+    private JMenuBar createTableMenuBar() {
+        final JMenuBar tableMenuBar = new JMenuBar();
         tableMenuBar.add(createFileMenu());
+        return tableMenuBar;
     }
 
     private JMenu createFileMenu() {
@@ -32,6 +54,70 @@ public class Table {
             }
         });
         fileMenu.add(openPGN);
+        final JMenuItem exitMenuItem = new JMenuItem("Exit");
+        exitMenuItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+        fileMenu.add(exitMenuItem);
         return fileMenu;
+    }
+
+    private class BoardPanel extends JPanel{
+        final List<TilePanel> boardTiles;
+
+        BoardPanel(){
+            super(new GridLayout(8,8));
+            this.boardTiles = new ArrayList<>();
+            for(int i = 0; i < BoardUtils.NUM_TILES; i++){
+                final TilePanel tilePanel = new TilePanel(this, i);
+                this.boardTiles.add(tilePanel);
+                add(tilePanel);
+            }
+            setPreferredSize(BOARD_PANEL_DIMENSION);
+        }
+    }
+
+    private class TilePanel extends JPanel{
+        private final int tileId;
+
+        TilePanel(final BoardPanel boardPanel,
+                  final int tileId){
+            super(new GridBagLayout());
+            this.tileId = tileId;
+            setPreferredSize(TILE_PANEL_DIMENSION);
+            assignTileColor();
+            assignTilePieceIcon(chessBoard);
+            validate();
+        }
+
+        private void assignTilePieceIcon(final Board board){
+            this.removeAll();
+            if(board.getTile(this.tileId).isTileOccupied()){
+                try {
+                    final BufferedImage image = ImageIO.read(new File(defaultPieceIconPath + board.getTile(this.tileId).getPiece().getPieceAlliance().toString().substring(0,1) +
+                    board.getTile(this.tileId).getPiece().toString() + ".gif"));
+                    add(new JLabel(new ImageIcon(image)));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        private void assignTileColor() {
+            if (BoardUtils.FIRST_ROW[this.tileId] ||
+                    BoardUtils.THIRD_ROW[this.tileId] ||
+                    BoardUtils.FIFTH_ROW[this.tileId] ||
+                    BoardUtils.SEVENTH_ROW[this.tileId]) {
+                setBackground(this.tileId % 2 == 0 ? lightTileColor : darkTileColor);
+            } else if(BoardUtils.SECOND_ROW[this.tileId] ||
+                    BoardUtils.FOURTH_ROW[this.tileId] ||
+                    BoardUtils.SIXTH_ROW[this.tileId]  ||
+                    BoardUtils.EIGHTH_ROW[this.tileId]) {
+                setBackground(this.tileId % 2 != 0 ? lightTileColor : darkTileColor);
+            }
+        }
     }
 }
